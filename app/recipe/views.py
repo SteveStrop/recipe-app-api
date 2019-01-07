90f237988ec0d0e36f85dab963f7d6cce0d488cb
SteveStrop <steve_tropical@yahoo.co.uk>
2019-01-07 07:33:14 +0000

    refactored recipe/views: by adding BaseRecipeAttributesViewSet

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,10 +5,21 @@ from core.models import Tag, Ingredient
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    """Manage tags in the database"""
+class BaseRecipeAttributesViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+    """Base viewset for user owned recipe attributes"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new attribute"""
+        serializer.save(user=self.request.user)
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage tags in the database"""
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
